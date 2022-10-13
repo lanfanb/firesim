6dc1433157521d2c1b9ba34caa91d0e4c64c2a35
Benjamin Morse <benjamin.morse@sifive.com>
2022-10-13 23:26:13 +0000

    After talk with David
    Scala bundles and IO are approaching correctness. still some errors. need to work on C

--- sim/firesim-lib/src/main/scala/bridges/PlusargsBridge.scala
+++ sim/firesim-lib/src/main/scala/bridges/PlusargsBridge.scala
@@ -10,22 +10,27 @@ import freechips.rocketchip.util.{DecoupledHelper}
 
 /**
   * Defines a condition under which the simulator should halt. 
-  * @param Message A string printed by the driver to indicate why the simulator is terminating.
+  * @param Message A string printed by the driver to indicate why the svimulator is terminating.
   * @param isError When true, instructs the driver to return a non-zero value when exiting under this condition.
 **/
 // case class TerminationCondition(isErr: Boolean, message : String)
 
-/** A seq of termination conditions one for each concurrently running program
- */
+// These are scala
+// Signature copied from chipyard/generators/rocket-chip/src/main/scala/util/PlusArg.scala
 case class PlusargsBridgeParams(
-  defaultValue: Int
+  name: String,
+  default: BigInt = 0,
+  docstring: String = "",
+  width: Int = 32
 )                
 
 class PlusargsBridgeTargetIO(params: PlusargsBridgeParams) extends Bundle {
   val clock = Input(Clock())
 //   val terminationCode = Input(UInt((log2Ceil((params.conditionInfo).size)).W))
 // add an? output which is the plusargs value
-  val valid = Input(Bool()) 
+  // val valid = Input(Bool()) 
+  val out = Output(UInt((params.width).W))
+  // val terminationCode = UInt((params.width).W)
 }
 
 class PlusargsBridgeHostIO(params: PlusargsBridgeParams)
@@ -35,7 +40,9 @@ class PlusargsBridgeHostIO(params: PlusargsBridgeParams)
   //It is just inputs from the target to indicate test completion
   //The HostPort annotation would not work with aggregates so creating individual channels.
 // //   val terminationCodeH = InputChannel(targetIO.terminationCode)
-  val validH = InputChannel(targetIO.valid)
+  // val validH = InputChannel(targetIO.valid)
+  // val validH = true.B
+  val outChannel = OutputChannel(targetIO.out)
 }
 
 class PlusargsBridge(params: PlusargsBridgeParams) extends BlackBox
@@ -50,16 +57,33 @@ class PlusargsBridge(params: PlusargsBridgeParams) extends BlackBox
 
 object PlusargsBridge {
 
-  private def annotatePlusargsBridge(clock: Clock, reset: Reset, conditionBools: Seq[Bool], params: PlusargsBridgeParams): PlusargsBridge = {
-    val finish = conditionBools.reduce(_||_)
-    val errMessageID = PriorityEncoder(conditionBools)
+  private def annotatePlusargsBridge(clock: Clock, reset: Reset, params: PlusargsBridgeParams): PlusargsBridge = {
+    // val finish = true.B // conditionBools.reduce(_||_)
+    // val errMessageID = PriorityEncoder(conditionBools)
     // require(params.conditionInfo.size==conditionBools.size)
-    val terminationBridgeTarget = Module(new PlusargsBridge(params))
-    // terminationBridgeTarget.io.terminationCode := errMessageID
-    terminationBridgeTarget.io.valid := finish && !reset.asBool
-    terminationBridgeTarget.io.clock := clock 
-    terminationBridgeTarget  
+    val target = Module(new PlusargsBridge(params))
+    // target.io.terminationCode := errMessageID
+    // target.io.valid := finish && !reset.asBool
+    target.io.clock := clock
+    target
   }
+
+  // Signature copied from chipyard/generators/rocket-chip/src/main/scala/util/PlusArg.scala
+  def apply(name: String, default: BigInt = 0, docstring: String = "", width: Int = 32): PlusargsBridge = {
+    val params = PlusargsBridgeParams(name, default, docstring, width)
+    annotatePlusargsBridge(Module.clock, Module.reset, params)
+
+  }
+
+  def apply(params: PlusargsBridgeParams): PlusargsBridge = {
+    annotatePlusargsBridge(Module.clock, Module.reset, params)
+  }
+
+  def drive(params: PlusargsBridgeParams): UInt = {
+    val target = annotatePlusargsBridge(Module.clock, Module.reset, params)
+    target.io.out
+  }
+
 
 /** Instatiates the target side of the Bridge, selects one of 
  *  the available finish conditions and passes on the corresponding 
@@ -70,15 +94,15 @@ object PlusargsBridge {
  *  @param params: Possible list of message info to be returned by simulator  
  */
 
-  def apply(clock: Clock, reset: Reset, conditionBools: Seq[Bool], params: PlusargsBridgeParams): PlusargsBridge = {
-    annotatePlusargsBridge(clock, reset, conditionBools, params)
-  }
+  // def apply(clock: Clock, reset: Reset, conditionBools: Seq[Bool], params: PlusargsBridgeParams): PlusargsBridge = {
+  //   annotatePlusargsBridge(clock, reset, conditionBools, params)
+  // }
 
 /** Simpler way to instantiate target side of the bridge by using implicit clock and reset
  */
-  def apply(conditionBools: Seq[Bool], params: PlusargsBridgeParams): PlusargsBridge = {
-    annotatePlusargsBridge(Module.clock, Module.reset, conditionBools, params)
-  }
+  // def apply(conditionBools: Seq[Bool], params: PlusargsBridgeParams): PlusargsBridge = {
+  //   annotatePlusargsBridge(Module.clock, Module.reset, conditionBools, params)
+  // }
 
 }
 
@@ -88,14 +112,14 @@ class PlusargsBridgeModule(params: PlusargsBridgeParams)(implicit p: Parameters)
     val io = IO(new WidgetIO())
     val hPort = IO(new PlusargsBridgeHostIO(params)())
     
-    val statusDone = Queue(hPort.validH)
+    val statusDone = true.B
     // // val terminationCode = Queue(hPort.terminationCodeH)
 
     val cycleCountWidth = 64
 
     val tokenCounter = genWideRORegInit(0.U(cycleCountWidth.W), "out_counter") 
 
-    val noTermination = !statusDone.bits
+    // val noTermination = !statusDone.bits
 
     // val tFireHelper = DecoupledHelper(terminationCode.valid, statusDone.valid, noTermination)
     // val tFireHelper = true.B;
@@ -117,7 +141,7 @@ class PlusargsBridgeModule(params: PlusargsBridgeParams)(implicit p: Parameters)
       import CppGenerationUtils._
       val headerWidgetName = getWName.toUpperCase
       super.genHeader(base, sb)
-      sb.append(genConstStatic(s"${headerWidgetName}_message_count", UInt32((params.defaultValue))))
+      sb.append(genConstStatic(s"${headerWidgetName}_message_count", UInt32((params.default))))
     //   sb.append(genConstStatic(s"${headerWidgetName}_message_count", UInt32((params.conditionInfo).size)))
     //   sb.append(genArray(s"${headerWidgetName}_message_type", (params.conditionInfo).map(x => UInt32(if(x.isErr) 1 else 0))))
     //   sb.append(genArray(s"${headerWidgetName}_message", (params.conditionInfo).map(x => CStrLit(x.message))))
--- sim/src/main/scala/midasexamples/PlusargsModule.scala
+++ sim/src/main/scala/midasexamples/PlusargsModule.scala
@@ -1,27 +1,68 @@
-
 //See LICENSE for license details.
 
 package firesim.midasexamples
 
 import chisel3._
 import freechips.rocketchip.config.Parameters
+import chisel3.util._
 import chisel3.util.Enum
 
+import midas.widgets._
+// import midas.widgets.{RationalClockBridge, PeekPokeBridge}
+
+// import midas.targetutils.SynthesizePrintf
+
+class PlusargsModuleIO(val params: PlusargsBridgeParams) extends Bundle {
+  // val msgInCycle = Input(UInt(16.W))  //Cycle when error is valid
+  // val validInCycle = Input(UInt(16.W)) //Cycle when the program finishes
+  // val doneErrCode = Input(UInt(((params.conditionInfo).size).W)) //Error code supplied by program
+  val in  = Input(Bool()) // delme
+  val gotPlusargValue = Output(UInt((params.width).W)) // Output value that plusargs bridge gives us
+  // val valid = Output(Bool()) // valid signal coming from plusargs bridge
+}
+
 class PlusargsDUT extends Module {
-  val io = IO(new Bundle {
-    val in  = Input(Bool())
-    val out = Output(Bool())
-  })
-//   val s_even :: s_odd :: Nil = Enum(2)
-//   val state  = RegInit(s_even)
-//   when (io.in) {
-//     when (state === s_even) { state := s_odd  }
-//     .otherwise              { state := s_even }
-//   }
-  // println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY")
-  io.out := false.B
+
+  // ----------------------------------------------------------------------
+  // val pin = PlusArg("custom_boot_pin", width=1)
+  // val endMessage = Seq(TerminationCondition(false, "success 1"), TerminationCondition(false, "success 2"), TerminationCondition(true, "failure 3"))
+  
+  val params = PlusargsBridgeParams("plusargs_test_value", default = 0, docstring = "", width=32)
+  // val params = PlusargsBridgeParams("plusargs_test_value")
+
+  val io = IO(new PlusargsModuleIO(params))
+  // val counter = RegInit(0.U(16.W))
+
+  // counter := counter + 1.U 
+  // val valid = (io.doneErrCode.asBools).map { _ && (counter >= io.validInCycle)}
+
+  // val bridge = PlusargsBridge(io.gotPlusargValue, params) 
+  // apply version 
+  // val bridge = PlusargsBridge(params)
+  // io.gotPlusargValue := bridge.io.out
+
+  io.gotPlusargValue := PlusargsBridge.drive(params)
+
 }
 
 
+// // A simple MIDAS harness that generates a legacy
+// // module DUT (it has a single io: Data member) and connects all of
+// // its IO to a PeekPokeBridge
+// class PeekPokeMidasExampleHarness2(dutGen: () => Module) extends RawModule {
+//   val clock = RationalClockBridge().io.clocks.head
+//   val reset = WireInit(false.B)
 
-class Plusargs(implicit p: Parameters) extends PeekPokeMidasExampleHarness(() => new ParityDUT)
+//   withClockAndReset(clock, reset) {
+//     val dut = Module(dutGen())
+//     val peekPokeBridge = PeekPokeBridge(clock, reset, chisel3.experimental.DataMirror.modulePorts(dut).filterNot {
+//       case (name, _) => name == "clock" | name == "reset"
+//     }:_*)
+//   }
+// }
+
+
+
+// class Plusargs(implicit p: Parameters) extends PeekPokeMidasExampleHarness(() => new ParityDUT)
+class Plusargs(implicit p: Parameters) extends PeekPokeMidasExampleHarness(() => new PlusargsDUT)
+// class Plusargs(implicit p: Parameters) extends PeekPokeMidasExampleHarness2(() => new PlusargsDUT)
